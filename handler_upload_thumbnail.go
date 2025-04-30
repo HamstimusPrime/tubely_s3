2d55ae05bd2e61ec5cd61997645a37e9ae387f0a
Mohammed Dadi <artofmohammed@gmail.com>
2025-04-30 17:35:32 +0100

    feat: implement thumbnail upload functionality
    - Parse multipart form data for thumbnail uploads
    - Store uploaded images in memory map
    - Update video metadata with thumbnail URL
    - Implement proper authorization checks
    - Connect upload handler to thumbnail retrieval system

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -31,6 +33,47 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 1 << 20
+	r.ParseMultipartForm(maxMemory)
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+	imgData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to fetch video", err)
+		return
+	}
+
+	if userID != video.CreateVideoParams.UserID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized access", err)
+		return
+	}
+
+	newThumbnail := thumbnail{
+		data:      imgData,
+		mediaType: mediaType,
+	}
+	videoThumbnails[video.ID] = newThumbnail
+
+	port := os.Getenv("PORT")
+	url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", port, videoID)
+	video.ThumbnailURL = &url
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to update video thumbnail", err)
+		return
+	}
 
 	respondWithJSON(w, http.StatusOK, struct{}{})
 }
